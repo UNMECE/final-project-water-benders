80a9f284e30a2a6d1785a1199ed1a1f5acfc4e21
Celeste Bivens <cbivens@unm.edu>
2025-12-11 21:45:27 -0700

    Final Solution

--- StudentSolution.cpp
+++ StudentSolution.cpp
@@ -50,17 +50,14 @@ void solveProblems(AcequiaManager& manager)
 
 
 /*Generally, gain can help determine whether a region is in drought or not. 
-
+In this case, our gain will be the difference or the "gap" waterneed and the waterlevel. 
+First, create our gain function.
 */
 double gain(Region *r)
 {
 	double gain = r->waterNeed - r->waterLevel;
 	return gain;
 }
-
-//THOUSANDS of gallons of water are being moved around. Therefore, we need to develop an algorithm that will convert the water difference to corresponding flow rate.
-//do I want rate to change each time? 
-
 
 void solveProblems(AcequiaManager& manager)
 {	
@@ -108,11 +105,10 @@ void solveProblems(AcequiaManager& manager)
 		// Canals are flowing in ONE DIRECTION. 
 		// First priority is the North region.
 
-
 		//------NORTH------- (Assuming North has the most water)
 		if(NorthGain > 0) // if north needs water
 		{
-			//open canal D 
+			//Open canal D 
 			canal_D -> toggleOpen(true);
 			canal_D->setFlowRate(rate);
 		} else 
@@ -121,9 +117,9 @@ void solveProblems(AcequiaManager& manager)
 		}
 
 		//-----SOUTH-------
-		if(SouthGain > 0) //if south needs water
+		if(SouthGain > 0) // If South needs water
 		{
-			//open canal A		
+			//Open canal A		
 			canal_A->toggleOpen(true);
 			canal_A->setFlowRate(rate);	
 		} else
@@ -132,9 +128,9 @@ void solveProblems(AcequiaManager& manager)
 		}
 
 		//-------EAST-------
-		if(EastGain > 0)// if East needs water
+		if(EastGain > 0)// If East needs water
 		{
-			//open canal C 
+			//Open canal C 
 			canal_C->toggleOpen(true);
 			canal_C->setFlowRate(rate);
 		} else 
@@ -142,8 +138,9 @@ void solveProblems(AcequiaManager& manager)
 			canal_C->toggleOpen(false);
 		}
 
-		if(EastGain > 0 && NorthGain <= NorthNeed)
+		if(EastGain > 0 && NorthGain <= NorthNeed)// If South needs more water 
 		{
+			//Open canal B
 			canal_B->toggleOpen(true);
 			canal_B->setFlowRate(rate);
 		}else
@@ -156,135 +153,3 @@ void solveProblems(AcequiaManager& manager)
 	}
 }
 
-/*
-double gain(Region *r)
-{
-	double gain = 0.0;
-	double difference = r->waterLevel - r->waterNeed;
-	return (difference > 0.0) ? difference: 0.0;
-}
-
-double deficit(Region *r)
-{
-	double deficit = 0.0;
-	double difference = r->waterNeed - r->waterLevel;
-	return (difference > 0.0) ? difference:0.0;
-}
-
-//THOUSANDS of gallons of water are being moved around. Therefore, we need to develop an algorithm that will convert the water difference to corresponding flow rate.
-double DetermineFlowRate(double RegionDeficit, double RegionGain)
-{
-	if(RegionDeficit <= 0.0 || RegionGain <= 0.0) return 0.0;
-
-	double amountToMove = abs(RegionDeficit - RegionGain); //in acre-feet
-	double rate = ((amountToMove * 325851)/3600) * 1000; //conversion to gal/s
-	if(rate > 1.0) rate = 1.0; // Capping rate => 1 gal/s
-	if(rate < 0.5) rate = 0.5; // Moderate flow
-	return rate;
-}
-
-
-void solveProblems(AcequiaManager& manager)
-{	
-	auto canals = manager.getCanals();
-	auto region = manager.getRegions();
-
-	//Assigning regions 
-	auto North = region[0];
-	auto NorthNeed = region[0]->waterNeed;
-
-	auto South = region[1];
-	auto SouthNeed = region[1]->waterNeed;
-
-	auto East = region[2];
-	auto EastNeed = region[2]->waterNeed;
-
-	//Assigning canal names based on initialization information from AcequiaManager.cpp
-	auto canal_A = canals[0]; //Connecting North -> South
-	auto canal_B = canals[1]; //Connecting South -> East
-	auto canal_C = canals[2]; //Connecting North -> East
-	auto canal_D = canals[3]; //Connecting East -> North
-
-
-	while(!manager.isSolved && manager.hour != manager.SimulationMax)
-	{
-		// Opening canals before each iteration 
-		for(auto canal : canals)
-		{
-			canal->toggleOpen(true);
-		}
-
-		// Updating current water level through each iteration 
-		auto NorthCurrent = North->waterLevel;
-		auto SouthCurrent = South->waterLevel;
-		auto EastCurrent = East->waterLevel;
-
-		auto NorthGain = gain(North);
-		auto SouthGain = gain(South);
-		auto EastGain = gain(East);
-
-		auto NorthDeficit = deficit(North);
-		auto SouthDeficit = deficit(South);
-		auto EastDeficit = deficit(East);
-
-		/*
-			defecit[0] = North
-			defecit[1] = South 
-			defecit[2] = East
-		*/
-		// WHAT ACEQUIAMANAGER PROVIDES US:
-		// North region is the key component for this system: it has canals connecting water to both South and East.
-		// Need to make sure North has enough water to distribute. 
-		// Canals are flowing in ONE DIRECTION. 
-		// First priority is the North region.
-/*
-		//------NORTH-------
-		if(NorthGain > 0.0 && SouthDeficit > 0)
-		{
-			//If South has a deficit, North sends water if it has gain through canal A
-			if(SouthDeficit > 0)
-			{
-				canal_A->setFlowRate(DetermineFlowRate(SouthDeficit, NorthGain));
-				//Stop once North reaches water level
-			}
-
-			//If East has a deficit, North sends water if it has gain through canal C
-			if(EastDeficit > 0)
-			{
-				canal_C->setFlowRate(DetermineFlowRate(EastDeficit, NorthGain));
-				//Stop once North reaches water level 
-			} 
-
-		} else if(NorthCurrent >= NorthNeed)
-		{
-			canal_C->toggleOpen(false);
-			canal_A->toggleOpen(false);
-		}
-
-		//-------EAST-------
-		if(EastGain > 0.0 && NorthDeficit > 0.0){
-			//if North has deficit, East sends water if it has gain through canal D
-			canal_D -> setFlowRate(DetermineFlowRate(NorthDeficit, EastGain));
-			//stop once North reaches 0 deficit. 
-		} else //if((NorthDeficit <= 0) && (EastCurrent > EastNeed))
-		{
-			canal_D -> toggleOpen(false);
-		}
-
-		//-----SOUTH-------
-		if(SouthGain > 0.0 && EastDeficit > 0.0)
-		{
-			//if East as deficit, South can send water if it has gain through  canal B
-			canal_B -> setFlowRate(DetermineFlowRate(EastDeficit, SouthGain));
-			//stop once east reaches 0 deficit.
-		} else //if((EastDeficit <= 0) && (SouthCurrent > SouthNeed))
-		{
-			//stop once South reaches water level
-			canal_B ->toggleOpen(false);
-		}
-
-		// Update for next hour of simulation 
-		manager.nexthour();
-	}
-}
-*/
